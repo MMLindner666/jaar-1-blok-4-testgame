59cfa2fa0adcd17143272590cb962998632d8bf1
Max Lindner <mm.lindner06@gmail.com>
2026-06-13 19:52:07 +0200

    Chance resources

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,10 +1,18 @@
-import { ImageSource, Sound, Resource, Loader } from 'excalibur'
+import { ImageSource, Sound, Resource, Loader, ImageWrapping } from 'excalibur'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-    Fish: new ImageSource('images/fish.png'),
-    Shark: new ImageSource('images/shark.png'),
-    Background: new ImageSource('images/water.jpg')
+    Player: new ImageSource('images/player.png'),
+    Gun: new ImageSource('images/gun.png'),
+    Bullet: new ImageSource('images/bullet.png'),
+    NormalZombie: new ImageSource('images/normal_zombie.png'),
+    BulkyZombie: new ImageSource('images/bulky_zombie.png'),
+    SpeedyZombie: new ImageSource('images/speedy_zombie.png'),
+    Wall: new ImageSource('images/wall.png'),
+    Background: new ImageSource('images/background.png', { wrapping: ImageWrapping.Repeat}),
+    HpFull: new ImageSource('images/hp_full.png'),
+    HpEmpty: new ImageSource('images/hp_empty.png'),
+    AmmoBox: new ImageSource('images/ammo_box.png')
 }
 
 
